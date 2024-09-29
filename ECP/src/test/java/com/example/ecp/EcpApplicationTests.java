0d8afc41dd8ff685e46548ff926074c79dc5186c
TGLap <97236246+TGLap@users.noreply.github.com>
2024-09-29 14:08:29 +0700

    Add files via upload

---
+++ ECP/src/test/java/com/example/ecp/EcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
